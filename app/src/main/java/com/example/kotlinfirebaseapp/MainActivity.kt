b0a5fce9064420f59feed7ae314ebe4e3d5ec16d
martin-ngigi <martinwainaina001@gmail.com>
2022-11-21 09:24:02 +0300

    Save Username and Profile Picture with Firebase Auth

--- app/src/main/java/com/example/kotlinfirebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfirebaseapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.kotlinfirebaseapp
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -21,12 +23,21 @@ class MainActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
+        /**
+         * to log out
+         */
+        //firebaseAuth.signOut()
+
         btnRegister.setOnClickListener{
             registerUser()
         }
 
         btnLogin.setOnClickListener{
             loginUser()
+        }
+
+        btnUpdateProfile.setOnClickListener{
+            updateProfile()
         }
 
 
@@ -72,12 +83,56 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun updateProfile(){
+        firebaseAuth.currentUser?.let { user ->
+            val username = etUsername.text.toString()
+            val photoURI = Uri.parse("android.resource://$packageName/${R.drawable.android3}")
+            val profileUpdates = UserProfileChangeRequest.Builder()
+                .setDisplayName(username)
+                .setPhotoUri(photoURI)
+                .build()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    /**
+                     * check if user is logged in or not
+                     */
+                    checkLoggedInState()
+
+                    user.updateProfile(profileUpdates).await()
+                    /**
+                     * Switching to main activity since it cant be accessed here
+                     */
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity, "Successfully updated user profile ", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                catch (e: Exception){
+                    /**
+                     * Switching to main activity since it cant be accessed here
+                     */
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity, "Error: ${e.message} ", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+
+    }
+
     private fun checkLoggedInState() {
-        if (firebaseAuth.currentUser == null){
+        val user = firebaseAuth.currentUser
+        if (user == null){
             tvLoggedIn.text = "You are not logged in "
         }
         else{
+            /**
+             * set data to ui
+             */
             tvLoggedIn.text = "You are logged in "
+            etUsername.setText(user.displayName)
+            ivProfilePicture.setImageURI(user.photoUrl)
         }
     }
 }
